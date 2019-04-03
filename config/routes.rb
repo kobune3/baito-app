cfd3f22086d4bc207f9381e79490617e6b5c432b
EC2 Default User <ec2-user@ip-172-31-19-176.ap-southeast-1.compute.internal>
2019-04-03 14:29:39 +0000

    企業へのレビューができるようになりました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "reviews#index"
-  resources :reviews
+  root "companies#index"
+  resources :companies do
+    resources :reviews, module: :companies
+  end
 end
