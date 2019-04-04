7e1e12173007069dbfd64015803b49b5a871670f
EC2 Default User <ec2-user@ip-172-31-19-176.ap-southeast-1.compute.internal>
2019-04-04 13:15:12 +0000

    change schema file

--- db/schema.rb
+++ db/schema.rb
@@ -27,18 +27,4 @@ ActiveRecord::Schema.define(version: 2019_04_02_142401) do
     t.datetime "updated_at", null: false
     t.index ["company_id", "user_id"], name: "index_reviews_on_company_id_and_user_id", unique: true
   end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
